6d3b3839e3e65d11bd4e39117045a978dcc8067f
Garrett Erven <gwerven@gmail.com>
2020-03-26 12:25:52 -0400

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        // Values below represents the generated table name when we scaffold the db
+        public DbSet<Value> Values { get; set; }
+    }
+}
